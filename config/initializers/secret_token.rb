0817e1095c2ec2aba9a02a99c4c73d5a7d1caa0d
Adam Derewecki <adam@causes.com>
2012-06-14 00:31:26 -0700

    initial commit; 'rails new reddit-clone'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditClone::Application.config.secret_token = '4b6b6f6ac4f1f5099e80607484e4917f63d6afca3d5b538f725cbffbfb37d39161c7e63af8452122437cb549a65bf19dfca59ad4fe4543844338ee4f28fa66a0'
