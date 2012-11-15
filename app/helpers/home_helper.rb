24da8f98cc1336d0aa8744cbd59b276264d13864
Adam Derewecki <adam@causes.com>
2012-11-15 14:30:30 -0800

    Fix overzealous block <a>

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,9 @@
 module HomeHelper
+  def link_to_story(story, opts={})
+    title = story.title.length < 255 ? story.title : "#{story.title}.."
+    link_to title, story.url, opts
+  end
+
   def link_to_story_poster(story)
     link_to story.user.handle,
             "http://www.facebook.com/profile.php?id=#{story.user.id}"
